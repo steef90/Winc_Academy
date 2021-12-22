bfd69b2a69aed814ee318c4579dde390acea77d9
Steef <93709661+steef90@users.noreply.github.com>
2021-12-22 07:09:25 +0100

    Create script.js

---
+++ JavaScript/JS1/script.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy students");
+
+//dit is een grote som
+/* twee grote sommen
+let maal = 1230941 * 1823794;
+
+console.log(maal);
+
+let deel = 637263/54;
+
+console.log(deel);*/
